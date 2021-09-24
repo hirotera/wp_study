114106ddd0a087112606e4eda535b10fa0fbbc81
hirotera <pomme-de-terre@hotmail.co.jp>
2021-09-24 23:43:05 +0900

    single.php仕上げ

--- single.php
+++ single.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
   <?php get_header(); ?>
 
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+
+  
 
   <?php get_template_part('includes/header'); ?>
 
